fd4435f46c82b96e917be8fc72aa14013c4c037e
Dan Krause <dan.krause@rackspace.com>
2013-10-02 22:41:32 -0500

    Initial commit

---
+++ board/wifisd/post-build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# $1 is the path to the rootfs - run before image building
+# use to modify rootfs before image is built
---
+++ board/wifisd/post-image.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# $1 is images dir - run just after image building
+# use to turn image into suitable initramfs3.gz
